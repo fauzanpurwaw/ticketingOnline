6f3d4a510a83e5086cc40aeb16b6e7c3ff05a53b
fauzanpurwaw <fauzanpurwaw@gmail.com>
2023-05-18 19:42:09 +0700

    up

--- components/blog.vue
+++ components/blog.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="blog w-full h-full flex justify-center items-center">
         <div class="blog-content flex lg:flex-nowrap flex-wrap justify-center items-center w-full h-full">
-            <div class="blog-img h-full w-full lg:mr-10 sm:px-0 px-10">
-                <img src="/img/blog-img.webp" alt="" class="">
+            <div class="blog-img h-full w-full flex justify-center lg:mr-10 sm:px-0">
+                <img src="/img/blog-img.webp" alt="" class="w-80">
             </div>
             <div class="blog-text w-full h-full text-left">
                 <h1 class="text-2xl font-semibold py-10">Pesan Tiket dan Rencanain Liburan Jadi Gampang</h1>
